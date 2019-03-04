05925e914534531d6c78763d36a4c7251524320d
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2019-03-04 17:17:28 +0100

    concurrent flow arrival and user configurable exporand. and seed

--- src/coordsim/main.py
+++ src/coordsim/main.py
@@ -1,17 +1,28 @@
 import argparse
 import simpy
+import random
 from coordsim.simulation import flowsimulator
 
 
 def main():
+    default_seed = 9999
+
     parser = argparse.ArgumentParser(description="Coordination-Simulation tool")
     parser.add_argument('-d', '--duration', required=True, default=None, dest="duration")
     parser.add_argument('-r', '--rate', required=False, default=None, dest="rate")
+    parser.add_argument('-s', '--seed', required=False, default=default_seed, dest="seed")
+    parser.add_argument('-rm', '--randmean', required=False, default=1.0, dest="rand_mean")
     args = parser.parse_args()
+
+
+    random.seed(args.seed)
     env = simpy.Environment()
-    nodes = [{"id": 1, "name": "NY", "type": "ingress"}, {"id": 1, "name": "DC", "type": "ingress"},
-             {"id": 1, "name": "PH", "type": "normal"}]
-    flowsimulator.start_simulation(env, nodes)
+    nodes = [{"id": 1, "name": "NY", "type": "ingress"}, {"id": 2, "name": "DC", "type": "ingress"},
+             {"id": 3, "name": "PH", "type": "normal"}]
+    print("Coordination-Simulation")
+    print("Using seed {} and using mean {}\n".format(args.seed, args.rand_mean))
+
+    flowsimulator.start_simulation(env, nodes, float(args.rand_mean))
     env.run(until=args.duration)
 
 
--- src/coordsim/simulation/flowsimulator.py
+++ src/coordsim/simulation/flowsimulator.py
@@ -1,16 +1,15 @@
 import random
 
 
-def generate_flow(env, node, rate, duration):
+def generate_flow(env, node, rand_mean):
     # Print flow arrivals, departures and waiting for flow to end (duration) at a pre-specified rate
     flow_id = 0
     while True:
-        print("Flow {}{} arrived at time {}".format(node["name"], flow_id, env.now))
-        yield env.timeout(duration)
-        print("Flow {}{} departed at time {}".format(node["name"], flow_id, env.now))
+        duration = random.randint(0, 3)
+        inter_arr_time = random.expovariate(rand_mean)
+        env.process(flow_arrival(env, node, duration, flow_id))
         flow_id += 1
-        yield env.timeout(rate)
-    
+        yield env.timeout(inter_arr_time)
     
 
 #Filter out non-ingree nodes 
@@ -22,9 +21,19 @@ def ingress_nodes(nodes):
     return ing_nodes
 
 
-def start_simulation(env, nodes, sim_rate=0):
+def flow_arrival(env, node, duration, flow_id):
+    print("Flow {}{} arrived at time {}".format(node["name"], flow_id, env.now))
+    yield env.timeout(duration)
+    print("Flow {}{} departed at time {}".format(node["name"], flow_id, env.now))
+
+ 
+def start_simulation(env, nodes, rand_mean=1.0, sim_rate=0):
+    print("Starting simulation")
+    print("Using nodes list {}\n".format(nodes))
     ing_nodes = ingress_nodes(nodes)
+
+    print("Total of {} ingress nodes available: {}\n".format(len(ing_nodes), ing_nodes))
     for node in ing_nodes:
-        duration = random.randint(0, 3)
-        rate = random.random()
-        env.process(generate_flow(env, node, rate, duration))
+        env.process(generate_flow(env, node, rand_mean))
+
+
