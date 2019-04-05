1889c24dba08b608eca250737fd6e8cd96d8b1d2
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2019-04-05 16:20:04 +0200

    some cleanup and enhancements to prev. solution to #11.

--- src/coordsim/network/scheduler.py
+++ src/coordsim/network/scheduler.py
@@ -1,5 +1,14 @@
 def flow_schedule():
-    flow_schedule = {'a': {'pop0': 0.3, 'pop1': 0.6, 'pop2': 0.1},
-                     'b': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2},
-                     'c': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2}}
+    flow_schedule = {'pop0':
+                     {'a': {'pop0': 0.4, 'pop1': 0.6, 'pop2': 0},
+                      'b': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2},
+                      'c': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2}},
+                     'pop1':
+                     {'a': {'pop0': 0.3, 'pop1': 0.6, 'pop2': 0.1},
+                      'b': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2},
+                      'c': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2}},
+                     'pop2':
+                     {'a': {'pop0': 0.1, 'pop1': 0.6, 'pop2': 0.3},
+                      'b': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2},
+                      'c': {'pop0': 0.6, 'pop1': 0.2, 'pop2': 0.2}}}
     return flow_schedule
--- src/coordsim/simulation/flowsimulator.py
+++ src/coordsim/simulation/flowsimulator.py
@@ -15,10 +15,10 @@ def generate_flow(env, node, sf_placement, sfc_list, rand_mean):
         flow_duration = random.randint(0, 3)
         # Exponentially distributed random inter arrival rate using a user set (or default) mean
         inter_arr_time = random.expovariate(rand_mean)
-        sfc = [sfc for sfc in sfc_list.keys()]
+        flow_id_str = "{}-{}".format(node.node_id, flow_id)
         flow_sfc = np.random.choice([sfc for sfc in sfc_list.keys()])
-        flow = Flow(flow_id, flow_sfc, flow_duration)
-        # Let flows arrive concurrently, no need to wait for one flow to depart for another to arrive.
+        flow = Flow(flow_id_str, flow_sfc, flow_duration)
+        # Generate flows and schedule them at ingress node
         env.process(schedule_flow(env, node, flow, sf_placement, sfc_list))
         flow_id += 1
         yield env.timeout(inter_arr_time)
@@ -33,46 +33,43 @@ def ingress_nodes(nodes):
     return ing_nodes
 
 
-# Flow arrival and departure function
+# Flow arrival and departure functions. Just logs that flow arrived and departed.
 def flow_arrival(env, node, flow):
-    if isinstance(node, str):
-        log.info("Flow {} arrived at {} at time {} - current sf {} - flow duration: {}".format(flow.flow_id, node, env.now, flow.current_sf, flow.duration))
-        flow.current_node = node
-    else: 
-        log.info("Flow {} arrived at {} at time {} - current sf {} - flow duration: {}".format(flow.flow_id, node.node_id, env.now, flow.current_sf, flow.duration))
-        flow.current_node = node.name
-    
-    #yield env.timeout(flow.flow_duration)
-    #log.info("Flow {} departed {} at time {} - flow duration: {}".format(flow.flow_id, node.name, env.now, flow.duration))
+    log.info(
+        "Flow {} processed at sf '{}' node {} at time {}"
+        .format(flow.flow_id, flow.current_sf, node, env.now))
+    flow.current_node = node
 
 
 def flow_departure(env, node, flow):
+    log.info("Flow {} departed {} at time {}".format(flow.flow_id, node, env.now))
 
-    log.info("Flow {} departed {} at time {} - flow duration: {}".format(flow.flow_id, node, env.now, flow.duration))
 
-
+# Schedule flows. This function takes the generated flow object at the ingress node and handles it according
+# to the requested SFC. We check if the SFC that is being requested is indeed within the schedule, otherwise
+# we log a warning and drop the flow.
+# The algorithm will check the flow's requested SFC, and will forward the flow through the network using the
+# SFC's list of SFs based on the LB rules that are provided through the scheduler's 'flow_schedule()'
+# function.
 def schedule_flow(env, node, flow, sf_placement, sfc_list):
-    log.info("Flow {} arrived at {} at time {} - Requesting {} - flow duration: {}".format(flow.flow_id, node.node_id, env.now, flow.sfc, flow.duration))
+    log.info(
+        "Flow {} generated. arrived at ingress node {} at time {} - Requesting {} - flow duration: {}"
+        .format(flow.flow_id, node.node_id, env.now, flow.sfc, flow.duration))
     schedule = scheduler.flow_schedule()
     sfc = sfc_list.get(flow.sfc, None)
     if sfc is not None:
         for sf in sfc_list[flow.sfc]:
-            schedule_sf = schedule[sf]
+            schedule_sf = schedule[node.node_id][sf]
             flow.current_sf = sf
-            if sf is None:
-                log.warning("No Scheduling rule for requested SF. Dropping flow {}".format(flow.flow_id))
-                break
-            else:
-                sf_nodes = [sch_sf for sch_sf in schedule_sf.keys()]
-                sf_probability = [prob for name, prob in schedule_sf.items()]
-                next_node = np.random.choice(sf_nodes, 1, sf_probability)[0]
-                flow_arrival(env, next_node, flow)
-                flow_departure(env, next_node, flow)
-        yield env.timeout(1)
+            sf_nodes = [sch_sf for sch_sf in schedule_sf.keys()]
+            sf_probability = [prob for name, prob in schedule_sf.items()]
+            next_node = np.random.choice(sf_nodes, 1, sf_probability)[0]
+            node.node_id = next_node
+            flow_arrival(env, next_node, flow)
+            yield env.timeout(flow.duration)
+            flow_departure(env, next_node, flow)
     else:
         log.warning("No Scheduling rule for requested SFC. Dropping flow {}".format(flow.flow_id))
-
-    
 
 
 def start_simulation(env, nodes, sf_placement, sfc_list, rand_mean=1.0, sim_rate=0):
