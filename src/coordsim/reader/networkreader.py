2b51d28ba4b6a11707ec838ac819cdf26575bd4b
Adnan Manzoor <adnan904@mail.uni-paderborn.de>
2019-04-16 22:03:37 +0200

    Refactor, improve network and placement representation using NetworkX #18

--- src/coordsim/reader/networkreader.py
+++ src/coordsim/reader/networkreader.py
@@ -1,7 +1,6 @@
 import networkx as nx
 from geopy.distance import vincenty
 import numpy as np
-from coordsim.network.node import Node
 import logging as log
 import yaml
 from collections import defaultdict
@@ -16,19 +15,26 @@ from collections import defaultdict
 
 
 # Open yaml file and pass data to other functions for procesing.
-def network_update(yaml_file):
+def network_update(yaml_file, network):
     with open(yaml_file) as yaml_stream:
         yaml_data = yaml.load(yaml_stream)
-    return get_placement(yaml_data), get_sfc(yaml_data), get_sf(yaml_data)
+    return get_placement(yaml_data, network), get_sfc(yaml_data), get_sf(yaml_data)
 
 
 # Get the placement from the yaml data.
-def get_placement(placement_data):
+def get_placement(placement_data, network):
     vnf_placements = defaultdict(list)
+    # Getting the placements
     for vnf in placement_data['placement']['vnfs']:
         node = vnf['node']
         vnf_name = vnf['name']
         vnf_placements[node].append(vnf_name)
+    # Updating the placements in the NetworkX Graph
+    for node in network.nodes().items():
+        node[1]['available_sf'] = {}
+        for sf in vnf_placements[node[0]]:
+            node[1]['available_sf'][sf] = 1
+
     return vnf_placements
 
 
@@ -50,30 +56,46 @@ def get_sf(sf_data):
 
 
 # Read the GraphML file and return list of nodes and edges.
-def read_network(file, node_cap=None, link_cap=None):
+def read_network(file, node_cap=None, link_fwd_cap=None, link_bkwd_cap=None):
     SPEED_OF_LIGHT = 299792458  # meter per second
     PROPAGATION_FACTOR = 0.77  # https://en.wikipedia.org/wiki/Propagation_delay
 
     if not file.endswith(".graphml"):
         raise ValueError("{} is not a GraphML file".format(file))
-    network = nx.read_graphml(file, node_type=int)
+    graphml_network = nx.read_graphml(file, node_type=int)
+    networkx_network = nx.DiGraph()
+
+    #  Setting the nodes of the NetworkX Graph
+    for n in graphml_network.nodes(data=True):
+        node_id = "pop{}".format(n[0])
+        cap = n[1].get("NodeCap", node_cap)
+        if cap == node_cap:
+            log.warning("Using default NodeCap for node: {}".format(n))
+        node_type = n[1].get("NodeType", "Normal")
+        node_name = n[1].get("label", None)
+        if cap is None:
+            raise ValueError("No NodeCap. set for node{} in file {} (as cmd argument or in graphml)".format(n, file))
+        # Adding a Node object as a Node in the NetworkX Graph
+        # {"id": node_id, "name": node_name, "type": node_type, "cap": cpu})
+        # Type of node. For now it is either "Normal" or "Ingress"
+        networkx_network.add_node(node_id, name=node_name, type=node_type, cap=cap, available_sf={})
 
     # set links
-    link_ids = [("pop{}".format(e[0]), "pop{}".format(e[1])) for e in network.edges]
+    # calculate link delay based on geo positions of nodes;
 
-    # calculate link delay based on geo positions of nodes; duplicate links for
-    # bidirectionality and create complete links array
-    edges = {}
-    for e in network.edges(data=True):
+    for e in graphml_network.edges(data=True):
         # Check whether LinkDelay value is set, otherwise default to -1
+        source = "pop{}".format(e[0])
+        target = "pop{}".format(e[1])
         link_delay = e[2].get("LinkDelay", -1)
-        link_cap = e[2].get("LinkCap", link_cap)
-        if (link_cap is None):
+        link_fwd_cap = e[2].get("LinkFwdCap", link_fwd_cap)
+        link_bkwd_cap = e[2].get("LinkBkwdCap", link_bkwd_cap)
+        if link_fwd_cap is None and link_bkwd_cap is None:
             raise ValueError("Link {} has incorrect or no capacity defined in graphml file.".format(e))
         delay = 0
         if link_delay == -1:
-            n1 = network.nodes(data=True)[e[0]]
-            n2 = network.nodes(data=True)[e[1]]
+            n1 = graphml_network.nodes(data=True)[e[0]]
+            n2 = graphml_network.nodes(data=True)[e[1]]
             n1_lat, n1_long = n1.get("Latitude"), n1.get("Longitude")
             n2_lat, n2_long = n2.get("Latitude"), n2.get("Longitude")
             distance = vincenty((n1_lat, n1_long), (n2_lat, n2_long)).meters  # in meters
@@ -81,30 +103,10 @@ def read_network(file, node_cap=None, link_cap=None):
             delay = int(np.around((distance / SPEED_OF_LIGHT * 1000) * PROPAGATION_FACTOR))  # in milliseconds
         else:
             delay = link_delay
-        edges[("pop{}".format(e[0]), "pop{}".format(e[1]))] = {"delay": delay, "cap": link_cap}
+        # delay = edge delay , cap = edge capacity in that direction
+        if e[2].get("LinkFwdCap") is not None:
+            networkx_network.add_edge(source, target, delay=delay, cap=link_fwd_cap)
+        if e[2].get("LinkBkwdCap") is not None:
+            networkx_network.add_edge(target, source, delay=delay, cap=link_bkwd_cap)
 
-    # add reversed links for bidirectionality
-    for e in network.edges(data=True):
-        e = ("pop{}".format(e[0]), "pop{}".format(e[1]))
-        e_reversed = (e[1], e[0])
-        link_ids.append(e_reversed)
-        edges[e_reversed] = edges[e]
-
-    links = []
-    for link in edges.keys():
-        links.append({"src": link[0], "dest": link[1], "delay": edges[link]["delay"], "cap": edges[link]["cap"]})
-    nodes = []
-    for n in network.nodes(data=True):
-        node_id = "pop{}".format(n[0])
-        cap = n[1].get("NodeCap", node_cap)
-        if cap == node_cap:
-            log.warning("Using default NodeCap for node: {}".format(n))
-        node_type = n[1].get("NodeType", "Normal")
-        node_name = n[1].get("label", None)
-        if (cap is None):
-            raise ValueError("No NodeCap. set for node{} in file {} (as cmd argument or in graphml)".format(n, file))
-        # Completing the nodes list with Node objects
-        nodes.append(Node(node_id, node_name, node_type, cap))
-        # nodes.append({"id": node_id, "name": node_name, "type": node_type, "cpu": cpu, "mem": mem})
-
-    return nodes, links
+    return networkx_network
