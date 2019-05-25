1f4f60b68808529dae466e0b10261a1aabe5f2f9
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2019-05-25 23:12:52 +0200

    fixed a minor mistake in networkreader.py

--- src/coordsim/reader/networkreader.py
+++ src/coordsim/reader/networkreader.py
@@ -148,7 +148,6 @@ def read_network(file, node_cap=None, link_cap=None):
                             "Now using default delay for edge: ({},{})".format(source, target))
             else:
                 distance = dist((n1_lat, n1_long), (n2_lat, n2_long)).meters  # in meters
-                distance = dist()
                 # round delay to int using np.around for consistency with emulator
                 delay = int(np.around((distance / SPEED_OF_LIGHT * 1000) * PROPAGATION_FACTOR))  # in milliseconds
         else:
