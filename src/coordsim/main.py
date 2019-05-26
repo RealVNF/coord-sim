2c9368023df3627bc010ad2212ce2c250aec558e
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2019-05-26 12:24:02 +0200

    Changed location of some arg parsing to 'parse_args()' in main.py

--- src/coordsim/main.py
+++ src/coordsim/main.py
@@ -21,7 +21,7 @@ def main():
     # Create a SimPy environment
     env = simpy.Environment()
     # Initialize environment (random seed and simpy.)
-    seed = int(args.seed)
+    seed = args.seed
     random.seed(seed)
 
     network = networkreader.read_network(args.network, node_cap=10, link_cap=10)
@@ -31,16 +31,16 @@ def main():
     log.info("Total of {} nodes have VNF's placed in them\n".format(len(sf_placement)))
 
     # Obtain flow datarate and size params
-    flow_dr_mean = float(args.flow_dr_mean)
-    flow_dr_stdev = float(args.flow_dr_stdev)
-    flow_size_shape = float(args.flow_size_shape)
+    flow_dr_mean = args.flow_dr_mean
+    flow_dr_stdev = args.flow_dr_stdev
+    flow_size_shape = args.flow_size_shape
 
     # Obtain flow inter arrival mean
-    inter_arr_mean = float(args.inter_arr_mean)
+    inter_arr_mean = args.inter_arr_mean
 
     schedule = scheduler.flow_schedule()
     # Simulation duration
-    duration = int(args.duration)
+    duration = args.duration
     params = SimulatorParams(network, sf_placement, sfc_list, sf_list, seed, schedule, inter_arr_mean=inter_arr_mean,
                              flow_dr_mean=flow_dr_mean, flow_dr_stdev=flow_dr_stdev, flow_size_shape=flow_size_shape)
     simulator = FlowSimulator(env, params)
@@ -54,15 +54,15 @@ def parse_args():
     default_seed = 9999
     # Read CLI arguments
     parser = argparse.ArgumentParser(description="Coordination-Simulation tool")
-    parser.add_argument('-d', '--duration', required=True, default=None, dest="duration")
+    parser.add_argument('-d', '--duration', required=True, default=None, dest="duration", type=int)
     parser.add_argument('-r', '--rate', required=False, default=None, dest="rate")
-    parser.add_argument('-s', '--seed', required=False, default=default_seed, dest="seed")
+    parser.add_argument('-s', '--seed', required=False, default=default_seed, dest="seed", type=int)
     parser.add_argument('-n', '--network', required=True, dest='network')
-    parser.add_argument('-iam', '--inter_arr_mean', required=False, default=1.0, dest="inter_arr_mean")
+    parser.add_argument('-iam', '--inter_arr_mean', required=False, default=1.0, dest="inter_arr_mean", type=float)
     parser.add_argument('-p', '--placement', required=True, default=None, dest="placement")
-    parser.add_argument('-fdm', '--flow_dr_mean', required=False, default=1.0, dest="flow_dr_mean")
-    parser.add_argument('-fds', '--flow_dr_stdev', required=False, default=1.0, dest="flow_dr_stdev")
-    parser.add_argument('-fss', '--flow_size_shape', required=False, default=1.0, dest="flow_size_shape")
+    parser.add_argument('-fdm', '--flow_dr_mean', required=False, default=1.0, dest="flow_dr_mean", type=float)
+    parser.add_argument('-fds', '--flow_dr_stdev', required=False, default=1.0, dest="flow_dr_stdev", type=float)
+    parser.add_argument('-fss', '--flow_size_shape', required=False, default=1.0, dest="flow_size_shape", type=float)
     return parser.parse_args()
 
 
