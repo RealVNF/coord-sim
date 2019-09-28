ef8453cb6de3f3827736a178b8dad657c8be5a38
Lars Dietrich Klenner <ldklenner@gmail.com>
2019-09-28 18:39:15 +0200

    Subprocess fix

--- src/algorithms/execution/subp.py
+++ src/algorithms/execution/subp.py
@@ -3,9 +3,9 @@ import subprocess
 def main():
 	scenarios = ['llc', 'lnc', 'hc']
 	run = 1
-	network = ['../../../params/networks/dfn_58.graphml', '../../../params/networks/intellifiber_73.graphml']
+	network = ['net_x', '../../../params/networks/dfn_58.graphml', '../../../params/networks/intellifiber_73.graphml']
 	ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
-	algo = ['g1, spr1, spr2']
+	algo = ['g1', 'spr1', 'spr2']
 
 	for s in scenarios:
 		for r in range(run):
@@ -13,7 +13,7 @@ def main():
 				for ing in ingress:
 					processes = []
 					for a in algo:
-						processes.append(subprocess.Popen(['python', "runner.py", s, r, net, ing, a]))
+						processes.append(subprocess.Popen(['python', "runner.py", s, str(r), net, ing, a]))
 					for p in processes:
 						p.wait()
 
