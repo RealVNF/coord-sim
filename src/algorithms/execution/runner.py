2d7a4911a764a1dbd48fad67e75995d4dd7a6310
Lars Dietrich Klenner <ldklenner@gmail.com>
2019-09-28 18:52:23 +0200

    Automatic simulation execution update (ASEU)

--- src/algorithms/execution/runner.py
+++ src/algorithms/execution/runner.py
@@ -1,5 +1,6 @@
 import logging
 import os
+
 import random
 import networkx as nx
 import numpy as np
@@ -11,6 +12,7 @@ from algorithms.greedy.g1 import G1Algo
 
 import sys
 import yaml
+import csv
 
 log = logging.getLogger(__name__)
 
@@ -18,9 +20,17 @@ log = logging.getLogger(__name__)
 def main():
     scenario = sys.argv[1]
     run = sys.argv[2]
-    network = sys.argv[3]
+    network_path = sys.argv[3]
+    network = os.path.basename(network_path)
     ingress = sys.argv[4]
     algo = sys.argv[5]
+
+    result_path = f'scenarios/{scenario}/{run}/{network}/{ingress}/{algo}'
+    os.makedirs(result_path, exist_ok=True)
+
+    with open(f'{result_path}/out.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow([1, 2, 3, 4, 5, 6])
 
     print(f'{scenario}-{run}-{os.path.basename(network)}-{ingress}-{algo}')
 
