5140a3ac0357cf1f84cb5e5fa3a985e2c878d90b
Lars Dietrich Klenner <ldklenner@gmail.com>
2019-09-28 18:33:59 +0200

    Subprocess corrections

--- src/algorithms/execution/subp.py
+++ src/algorithms/execution/subp.py
@@ -4,7 +4,7 @@ def main():
 	scenarios = ['llc', 'lnc', 'hc']
 	run = 1
 	network = ['../../../params/networks/dfn_58.graphml', '../../../params/networks/intellifiber_73.graphml']
-	ingress = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+	ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
 	algo = ['g1, spr1, spr2']
 
 	for s in scenarios:
