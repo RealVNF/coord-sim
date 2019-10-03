8692cff92453a64c0e9a317bcbca781cd1a15638
Lars Dietrich Klenner <ldklenner@gmail.com>
2019-10-03 18:04:05 +0200

    Aggregator wip

--- src/algorithms/execution/3x3/aggregator.py
+++ src/algorithms/execution/3x3/aggregator.py
@@ -3,11 +3,42 @@ import os
 
 scenarios = ['llc', 'lnc', 'hc']
 runs = ['0']
-networks = ['net_x', 'dfn_58.graphml', 'intellifiber_73.graphml']
+networks = ['bics_34.graphml', 'dfn_58.graphml', 'intellifiber_73.graphml']
 ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
 algos = ['g1', 'spr1', 'spr2']
-metrics = [0, 1, 2, 3, 4, 5]
-metrics_id = {0: 'ab', 1: 'cd', 2: 'ef', 3: 'gh', 4: 'ij', 5: 'kl'}
+
+metrics2index = {'time': 0,
+                 'total_flows': 1,
+                 'successful_flows': 2,
+                 'dropped_flows': 3,
+                 'in_network_flows': 4,
+                 'avg_end2end_delay_of_dropped_flows': 5,
+                 'avg_end2end_delay_of_processed_flows': 6,
+                 'avg_sf_processing_delay': 7,
+                 'avg_sfc_length': 8,
+                 'avg_crossed_link_delay': 9,
+                 'avg_path_delay': 10,
+                 'avg_path_delay_of_processed_flows': 11,
+                 'avg_ingress_2_egress_path_delay_of_processed_flows': 12,
+                 'avg_node_load': 13,
+                 'avg_link_load': 14
+                 }
+index2metric = {0: 'time',
+                1: 'total_flows',
+                2: 'successful_flows',
+                3: 'dropped_flows',
+                4: 'in_network_flows',
+                5: 'avg_end2end_delay_of_dropped_flows',
+                6: 'avg_end2end_delay_of_processed_flows',
+                7: 'avg_sf_processing_delay',
+                8: 'avg_sfc_length',
+                9: 'avg_crossed_link_delay',
+                10: 'avg_path_delay',
+                11: 'avg_path_delay_of_processed_flows',
+                12: 'avg_ingress_2_egress_path_delay_of_processed_flows',
+                13: 'avg_node_load',
+                14: 'avg_link_load'
+                }
 
 
 def read_output_file(path):
@@ -34,11 +65,12 @@ def collect_data():
                 for ing in ingress:
                     data[s][r][net][ing] = {}
                     for a in algos:
-                        data[s][r][net][ing][a] = get_last_row(read_output_file(f'scenarios/{s}/{r}/{net}/{ing}/{a}/out.csv'))
+                        data[s][r][net][ing][a] = get_last_row(
+                            read_output_file(f'scenarios/{s}/{r}/{net}/{ing}/{a}/out.csv'))
     return data
 
 
-def avgerage_data(data):
+def average_data(data):
     avg_data = {}
     for s in scenarios:
         avg_data[s] = {}
@@ -48,7 +80,7 @@ def avgerage_data(data):
                 avg_data[s][net][ing] = {}
                 for a in algos:
                     avg_data[s][net][ing][a] = []
-                    for m in metrics:
+                    for m in range(15):
                         sum = 0
                         for r in runs:
                             sum += int(data[s][r][net][ing][a][m])
@@ -56,25 +88,23 @@ def avgerage_data(data):
     return avg_data
 
 
-def collect_series(avg_data):
-    data = {}
+def transform_data(data, metric_set, metric_set_id):
+    os.makedirs(f'transformed/{metric_set_id}', exist_ok=False)
+
     for s in scenarios:
-        data[s] = {}
         for net in networks:
-            data[s][net] = {}
-            for m in metrics:
-                series = {a: [] for a in algos}
+            for a in algos:
                 for ing in ingress:
-                    for a in algos:
-                        series[a].append(avg_data[s][net][ing][a][m])
-                data[s][net][metrics_id[m]] = series
-    return data
+                    with open(f'transformed/{metric_set_id}/{s}/{net}/t-metrics.csv', 'a+') as csvfile:
+                        for m in metric_set:
+                            row = [ing, data[s][net][ing][a][metrics2index[m]], f'{a}-{m}']
+                            writer = csv.writer(row)
 
 
 def main():
     data = collect_data()
-    avg_data = avgerage_data(data)
-    series = collect_series(avg_data)
+    avg_data = average_data(data)
+    transform_data(avg_data, ['total_flows', 'successful_flows'], 'flows')
     print('')
 
 
--- src/algorithms/execution/3x3/subp.py
+++ src/algorithms/execution/3x3/subp.py
@@ -1,27 +1,29 @@
 import subprocess
-from timeit import default_timer as timer
 from datetime import timedelta
+from timeit import default_timer as timer
+
 
 def main():
-	scenarios = ['llc', 'lnc', 'hc']
-	runs = ['0']
-	networks = ['net_x', '../../../../params/networks/dfn_58.graphml', '../../../../params/networks/intellifiber_73.graphml']
-	ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
-	algos = ['gpasp', 'spr1', 'spr2']
+    scenarios = ['llc', 'lnc', 'hc']
+    runs = ['0']
+    networks = ['../../../../params/bics_34.graphml', '../../../../params/networks/dfn_58.graphml',
+                '../../../../params/networks/intellifiber_73.graphml']
+    ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
+    algos = ['gpasp', 'spr1', 'spr2']
 
-	start = timer()
-	for s in scenarios:
-		for r in runs:
-			for net in networks:
-				for ing in ingress:
-					processes = []
-					for a in algos:
-						processes.append(subprocess.Popen(['python', "runner.py", s, r, net, ing, a]))
-					for p in processes:
-						p.wait()
-	end = timer()
-	print(timedelta(seconds=end - start))
+    start = timer()
+    for s in scenarios:
+        for r in runs:
+            for net in networks:
+                for ing in ingress:
+                    processes = []
+                    for a in algos:
+                        processes.append(subprocess.Popen(['python', "runner.py", s, r, net, ing, a]))
+                    for p in processes:
+                        p.wait()
+    end = timer()
+    print(timedelta(seconds=end - start))
 
 
 if __name__ == "__main__":
-	main()
+    main()
