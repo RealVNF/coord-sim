9a4c8f9587dba1699aa8694fd89471946637ace0
ich <schellenberg-r@rambler.ru>
2020-08-29 22:50:01 +0200

    simple cli

--- animations/animations.py
+++ animations/animations.py
@@ -1,5 +1,8 @@
 from matplotlib.animation import FuncAnimation, ArtistAnimation
 from geopy.distance import distance as dist
+from matplotlib.widgets import Slider
+from argparse import ArgumentParser
+from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
@@ -11,19 +14,20 @@ matplotlib.use('TkAgg')
 
 class PlacementAnime:
 
-    def __init__(self, test_dir=None):
+    def __init__(self, test_dir=".", placement_file="placements.csv", rl_state_file="rl_state.csv",
+                 resources_file="resources.csv"):
         self.test_dir = test_dir
-        self.network_file = "line-in1-cap2-delay10.graphml"
-        self.placement_file = "placements.csv"
-        self.rl_state_file = "rl_state.csv"
-        self.resources_file = "resources.csv"
+        self.network_file = self.get_network_filename()
+        self.placement_file = os.path.join(test_dir, placement_file)
+        self.rl_state_file = os.path.join(test_dir, rl_state_file)
+        self.resources_file = os.path.join(test_dir, resources_file)
+
         self.net_x = networkx.read_graphml(self.network_file)
         self.set_linkDelay()
         self.placement = pd.read_csv(self.placement_file)
         self.placement = self.placement.groupby(["time"])
         self.ingress_traffic = pd.read_csv(self.rl_state_file, header=None)
         self.ingress_traffic.columns = ["episode", "time"]+[f"pop{i}" for i in range(self.net_x.number_of_nodes())]
-        print(self.ingress_traffic)
         self.node_pos = self.determine_node_pos()
         self.edge_pos = self.determine_edge_pos()
         self.extent_offset = 5
@@ -34,18 +38,29 @@ class PlacementAnime:
         self.component_offsets_y = 1
         self.ingress_node_colors = {"pop0": "b", "pop1": "y", "pop2": "g"}
         self.last_point = {"pop0": [0, 0], "pop1": [0, 0], "pop2": [0, 0]}
-        self.fig, self.ax = plt.subplots(2, 1)
-        self.ax, self.ing_traffic_ax = self.ax
+
+        self.fig = plt.figure()
+        #self.ax = plt.subplots(2, 1)
+        self.ax, self.ing_traffic_ax = self.fig.add_subplot(211), self.fig.add_subplot(212)
         self.ln = plt.plot([], [])
         self.ln.extend(self.draw_network())
         self.ln.extend(self.plot_capacity())
         self.ln.extend(self.plot_delay())
         #self.time_label = plt.text(self.axis_extent[0, 0] + 1, self.axis_extent[1, 0] + 1, "0")
         #self.ln.append(self.time_label)
+        self.animation = None
+        self.artists = []
+        self.moments = []
+        self.ln_ingress = []
 
-    def get_filenames(self):
+        if not test_dir == ".":
+            self.video_filename = os.path.basename(os.path.dirname(test_dir))
+        else:
+            self.video_filename = "animation_video"
+
+    def get_network_filename(self):
         listdir = os.listdir(self.test_dir)
-        self.network_file = filter(lambda f: ".graphml")
+        return os.path.join(self.test_dir, list(filter(lambda f: ".graphml" in f, listdir))[0])
 
     def draw_network(self):
         ln = plt.plot([], [])
@@ -141,6 +156,23 @@ class PlacementAnime:
         self.ing_traffic_ax.set_ylim([0, 0.5])
         return self.ln
 
+    def plot_moment(self, frame):
+        ln2 = plt.plot([], [])
+        # self.time_label._text = str(frame)
+        # print("changed: ", self.time_label)
+        ln2.extend(self.plot_components(frame))
+        ln2.append(self.ax.text(self.axis_extent[0, 0] + 1, self.axis_extent[1, 0] + 1, str(frame)))
+        #self.ln_ingress.extend(self.plot_ingress_traffic(frame))
+        #ln2.extend(self.ln_ingress)
+        return ln2
+
+    def create_moments(self):
+        ln = []
+        self.init()
+        for frame in self.placement.groups:
+            ln.append(self.plot_moment(frame))
+        self.moments.extend(ln)
+
     def update(self, frame):
         lns = plt.plot([], [])
         ln2 = plt.plot([], [])
@@ -160,25 +192,81 @@ class PlacementAnime:
             ln.append(self.update(frame))
         return ln
 
+    def create_animation(self):
+        self.artists = [self.ln]
+        self.artists.extend(self.create_artists())
+        self.animation = ArtistAnimation(self.fig, self.artists, interval=100, repeat=False)
 
 
-#print(type(placement.get_group(100)))
+class PlacementAnimesManager:
+
+    def __init__(self, results_dir):
+        self.cur_anime = None
+        self.animes = []
+        for root, subdirs, files in os.walk(results_dir):
+            if len(files) != 0 and "test" in root:
+                self.animes.append(root)
+
+    def load_animation(self, root):
+        self.cur_anime = PlacementAnime(root)
+        self.cur_anime.create_animation()
+        return self.cur_anime
 
 
-#im = plt.imread("abilene-rand-cap.jpg", )
+def parse_args(args=None):
+    parser = ArgumentParser()
+    parser.add_argument("--results_dir", default=None)
+    parser.add_argument("--test_dir", default=None)
+    #parser.add_argument("-a", "--timestamp_seed_agent", default=None, dest="timestamp_seed_agent")
+    #parser.add_argument("-t", "--timestamp_seed_test", default=None, dest="timestamp_seed_test")
+    parser.add_argument("-st", "--show_tests", default=False, action="store_true", dest="show_tests")
+    parser.add_argument("--show", default=False, action="store_true")
+    parser.add_argument("--save", default=False, action="store_true")
+    return vars(parser.parse_args(args))
 
-#implot = ax.imshow(im, extent=extent)
 
-Writer = matplotlib.animation.writers['ffmpeg']
-writer = Writer(fps=15, bitrate=1800)
-pa = PlacementAnime()
-artists = [pa.ln]
-artists.extend(pa.create_artists())
-#ani = FuncAnimation(pa.fig, pa.update, frames=list(pa.placement.groups), init_func=pa.init, blit=True, repeat=False)
-ani = ArtistAnimation(pa.fig, artists)
+def list_tests(results_dir):
+    animes = []
+    for root, subdirs, files in os.walk(results_dir):
+        if len(files) != 0 and "test" in root:
+            animes.append(root)
+    return animes
 
-#ani.save('lines.mp4', writer=writer)
-plt.show()
-"""html = ani.to_html5_video()
-with open('ani.html', 'w') as f:
-    f.write(html)"""
+
+def main(args=None):
+    args = parse_args(args)
+    tests = None
+    if args["results_dir"]:
+        tests = list_tests(args["results_dir"])
+    print(args)
+    if args["show_tests"]:
+        pprint(tests)
+    else:
+        if not args["test_dir"]:
+            args["test_dir"] = tests[0]
+        pa = PlacementAnime(args["test_dir"])
+        pa.create_animation()
+        if args["show"]:
+            plt.show()
+        if args["save"]:
+            print(f'{pa.video_filename}.html')
+            html = pa.animation.to_html5_video()
+            with open(f'{pa.video_filename}.html', 'w') as f:
+                f.write(html)
+
+    # plt.show()
+    #list(pam.animes.values())[0].fig.show()
+
+
+if __name__ == "__main__":
+    #main(["--test_dir", ".", "--show"])
+    main()
+    """pa = PlacementAnime()
+    artists = [pa.ln]
+    artists.extend(pa.create_artists())
+    ani = ArtistAnimation(pa.fig, artists, interval=1, repeat=False)
+
+    plt.show()"""
+    """html = ani.to_html5_video()
+    with open('ani.html', 'w') as f:
+        f.write(html)"""
