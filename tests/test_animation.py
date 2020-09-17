1060701c2cbc3b83da4a0d086e8e9d080d7a3b75
Rafael Schellenberg <schellenberg-r@rambler.ru>
2020-09-17 09:39:54 +0200

    remove print

--- tests/test_animation.py
+++ tests/test_animation.py
@@ -6,5 +6,4 @@ import os
 class TestAnimation(TestCase):
 
     def test_animation(self):
-        print(os.getcwd())
-        print(check_output(["animation", "--test_dir", "params/test_data/test-2020-07-22_21-33-20_seed7841"]))
+        check_output(["animation", "--test_dir", "params/test_data/test-2020-07-22_21-33-20_seed7841"])
