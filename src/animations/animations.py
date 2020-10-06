a9581f9ef171afa3b142a4a960ade9c934b679d5
Rafael Schellenberg <schellenberg-r@rambler.ru>
2020-10-06 14:11:35 +0200

    remove TkAgg

--- src/animations/animations.py
+++ src/animations/animations.py
@@ -8,7 +8,6 @@ import networkx
 import pandas as pd
 import os
 import yaml
-import matplotlib
 
 # https://stackoverflow.com/questions/40233986/python-is-there-a-function-or-formula-to-find-the-complementary-colour-of-a-rgb
 # Sum of the min & max of (a, b, c)
