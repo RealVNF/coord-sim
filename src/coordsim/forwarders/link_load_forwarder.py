e35b1b21788470c1cce8adf5211616d1a49f715b
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2020-11-12 21:47:23 +0100

    removed irrelevant link_load module

--- src/coordsim/forwarders/link_load_forwarder.py
+++
@@ -1,9 +0,0 @@
-class LinkLoadForwarder(object):
-    """
-    This is the default forwarder class. It takes the shortest path.
-    """
-    def __init__(self):
-        """
-        docstring
-        """
-        pass
