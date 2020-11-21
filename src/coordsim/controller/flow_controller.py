cdde9389ea8c0e7d96e027eb0b1bfb7cb5a64e1d
Haydar Qarawlus <qarawlus@mail.upb.de>
2020-11-22 00:19:10 +0100

    Moved duplicate functions to base class

--- src/coordsim/controller/flow_controller.py
+++ src/coordsim/controller/flow_controller.py
@@ -20,7 +20,7 @@ class SPRState:
 
 class FlowController(BaseController):
     """
-    This is the default decision maker class. It makes flow decisions based on the scheduling table
+    This is a flow controller class. It makes per-flow decisions based on an external decision
     """
 
     def __init__(self, env, params, simulator):
@@ -42,8 +42,8 @@ class FlowController(BaseController):
         return simulator_state
 
     def get_next_state(self, action: SimulatorAction) -> SimulatorState:
-        """ Apply a decision and run until a specified duration has finished
-        If init, create initial state
+        """
+        Apply a decision and run until next flow arrives
         """
 
         # self.writer.write_action_result(self.episode, self.env.now, action)
@@ -90,75 +90,6 @@ class FlowController(BaseController):
         # Create a new SimulatorState object to pass to the RL Agent
         simulator_state = SPRState(flow, self.params.network, self.params.sfc_list, self.network_stats)
         return simulator_state
-
-    def update_prediction(self):
-        requested_traffic = self.get_current_ingress_traffic()
-        self.predictor.predict_traffic(self.env.now, current_traffic=requested_traffic)
-        stats = self.params.metrics.get_metrics()
-        self.traffic = stats['run_total_requested_traffic']
-
-    def parse_network(self) -> dict:
-        """
-        Converts the NetworkX network in the simulator to a dict in a format specified in the SimulatorState class.
-        """
-        max_node_usage = self.params.metrics.get_metrics()['run_max_node_usage']
-        self.network_dict = {'nodes': [], 'edges': []}
-        for node in self.params.network.nodes(data=True):
-            node_cap = node[1]['cap']
-            run_max_node_usage = max_node_usage[node[0]]
-            # 'used_resources' here is the max usage for the run.
-            self.network_dict['nodes'].append({'id': node[0], 'resource': node_cap,
-                                               'used_resources': run_max_node_usage})
-        for edge in self.params.network.edges(data=True):
-            edge_src = edge[0]
-            edge_dest = edge[1]
-            edge_delay = edge[2]['delay']
-            edge_dr = edge[2]['cap']
-            # We use a fixed user data rate for the edges here as the functionality is not yet incorporated in the
-            # simulator.
-            # TODO: Implement used edge data rates in the simulator.
-            edge_used_dr = 0
-            self.network_dict['edges'].append({
-                'src': edge_src,
-                'dst': edge_dest,
-                'delay': edge_delay,
-                'data_rate': edge_dr,
-                'used_data_rate': edge_used_dr
-            })
-
-    def get_current_ingress_traffic(self) -> float:
-        """
-        Get current ingress traffic for the LSTM module
-        Current limitation: works for 1 SFC and 1 ingress node
-        """
-        # Get name of ingress SF from first SFC
-        first_sfc = list(self.params.sfc_list.keys())[0]
-        ingress_sf = self.params.sfc_list[first_sfc][0]
-        ingress_node = self.params.ing_nodes[0][0]
-        ingress_traffic = self.metrics.metrics['run_total_requested_traffic'][ingress_node][first_sfc][ingress_sf]
-        return ingress_traffic
-
-    def network_metrics(self):
-        """
-        Processes the metrics and parses them in a format specified in the SimulatorState class.
-        """
-        stats = self.params.metrics.get_metrics()
-        self.traffic = stats['run_total_requested_traffic']
-        self.network_stats = {
-            'processed_traffic': stats['run_total_processed_traffic'],
-            'total_flows': stats['generated_flows'],
-            'successful_flows': stats['processed_flows'],
-            'dropped_flows': stats['dropped_flows'],
-            'run_successful_flows': stats['run_processed_flows'],
-            'run_dropped_flows': stats['run_dropped_flows'],
-            'run_dropped_flows_per_node': stats['run_dropped_flows_per_node'],
-            'in_network_flows': stats['total_active_flows'],
-            'avg_end2end_delay': stats['avg_end2end_delay'],
-            'run_avg_end2end_delay': stats['run_avg_end2end_delay'],
-            'run_max_end2end_delay': stats['run_max_end2end_delay'],
-            'run_avg_path_delay': stats['run_avg_path_delay'],
-            'run_total_processed_traffic': stats['run_total_processed_traffic']
-        }
 
     def update_vnf_active_status(self):
         """ Update the VNF status in nodes and remove inactive VNFs """
